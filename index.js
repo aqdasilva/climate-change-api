1707fcf8958badb34b9421621a23c35e57a3fe62
aqdasilva <antqdasilva@gmail.com>
2023-01-23 18:01:22 -0500

    scraped 1 website that all have href with "climate"

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ const PORT = 8008
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const {response} = require("express");
 
 const app = express()
+
+const articles = []
+
+app.get('/',(req,res) => {
+    res.json('welcome to my climate change new api stuff')
+})
+
+app.get('/news', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) =>{
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+              const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch((err) => console.log(err))
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
